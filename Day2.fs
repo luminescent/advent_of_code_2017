fc9edb677310d48b31a2ef4ee806e8dc3161d015
luminescent <christina@doctorlink.com>
2017-12-05 21:11:45 +0000

    Day 1 and 2

---
+++ Day2.fs
@@ -0,0 +1,63 @@
+namespace AdventOfCode
+
+open System
+open System.IO
+open System.Runtime.InteropServices
+open System.Threading
+open System.Xml.Linq
+open System.Xml
+open System.Collections.Generic
+open System.Security.Cryptography.X509Certificates
+
+module Day2 = 
+
+    let rowDiff row = 
+        (row
+        |> Array.max) 
+        - 
+        (row
+        |> Array.min) 
+
+    let parseLine (line: string) =
+        line
+            .Split('\t', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun s -> Convert.ToInt32(s))
+
+    let parseLines lines = 
+        lines 
+        |> Array.map parseLine
+
+    let computeChecksum lines = 
+        lines
+        |> parseLines
+        |> Array.sumBy rowDiff
+
+    let rec rowDivision row = 
+        match row with
+        | head :: tail -> 
+            let possibleSmaller = tail |> List.tryFind(fun a -> a < head && a <> 0 && head % a = 0)
+            match possibleSmaller with 
+            | Some x -> head / x 
+            | None -> 
+                let possibleBigger = tail |> List.tryFind(fun a -> a > head && a % head = 0)
+                match possibleBigger with
+                | Some x -> x / head
+                | None -> rowDivision tail 
+        | [] -> failwithf "Unable to find divisors for row %A" row
+
+    let computeSumOfDivision lines = 
+        lines
+        |> parseLines
+        |> Array.map Array.toList
+        |> Array.sumBy rowDivision
+
+    let runDay2() = 
+        let lines = File.ReadAllLines "Day2.txt"
+
+        lines
+        |> computeChecksum
+        |> Console.WriteLine
+
+        lines 
+        |> computeSumOfDivision 
+        |> Console.WriteLine    
---
+++ Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AdventOfCode.Day1
+open AdventOfCode.Day2
+
+[<EntryPoint>]
+let main argv =
+    
+    runDay2()
+
+    0 // return an integer exit code
